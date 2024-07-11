5a7c4554a429f72a7094f42c2dde3ca924788450
ayevtushenko <alexey.yevtushenko@ubisoft.com>
2024-07-11 08:04:17 +0200

    Sketched out postgres backend

--- pkg/kvs/db_transactions.go
+++ pkg/kvs/db_transactions.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -32,15 +33,118 @@ func newDBTransactionLogger(settings PostgresSettings) (*PostgresTransactionLogg
 		return nil, err
 	}
 
-	err = db.Ping()
+	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
+	defer cancel()
+	err = db.PingContext(ctx)
 	if err != nil {
+		// NOTE: Almost always use %w instead of %v and %s for error formating
 		// https: //stackoverflow.com/questions/61283248/format-errors-in-go-s-v-or-w
 		return nil, fmt.Errorf("failed to establish db connection %w", err)
 	}
 
 	logger := &PostgresTransactionLogger{db: db}
 
+	if err := logger.createTablesIfDontExist(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
 	return logger, nil
+}
+
+func (l *PostgresTransactionLogger) dropTable(table string) error {
+	query := fmt.Sprintf(`DROP TABLE IF EXISTS %s;`, table)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if _, err := l.db.ExecContext(ctx, query, table); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *PostgresTransactionLogger) createTablesIfDontExist() error {
+	{
+		ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
+		defer cancel()
+
+		// NOTE: If the value is a map we should store it in a separate table,
+		// We can create a table for ints floats and maps
+		// id | event_type(PUT/GET/DELETE) | storage_type(Int/Float/String/Map)
+		query := `CREATE TABLE IF NOT EXISTS kvs_transactions_table (
+			id INTEGER PRIMARY KEY,
+			event_type INTEGER,
+			storage_type INTEGER
+		);`
+		_, err := l.db.ExecContext(ctx, query)
+		if err != nil {
+			return fmt.Errorf("failed to create transactions table %w", err)
+		}
+	}
+	{
+		query := `CREATE TABLE IF NOT EXISTS integers_table (
+			id INTEGER PRIMARY KEY,
+			event_type VARCHAR(64),
+			time_stamp TIMESTAMP,
+			key TEXT UNIQUE,
+			value INTEGER
+		);`
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		_, err := l.db.ExecContext(ctx, query)
+		if err != nil {
+			return fmt.Errorf("failed to crete integers table %w", err)
+		}
+	}
+	{
+		query := `CREATE TABLE IF NOT EXISTS floats_table (
+			id INTEGER PRIMARY KEY,
+			event_type VARCHAR(64),
+			time_stamp TIMESTAMP,
+			key TEXT UNIQUE,
+			value FLOAT
+		);` // Maybe use double precision?
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		_, err := l.db.ExecContext(ctx, query)
+		if err != nil {
+			return fmt.Errorf("failed to crete floats table %w", err)
+		}
+	}
+	{
+		query := `CREATE TABLE IF NOT EXISTS strings_table (
+			id INTEGER PRIMARY KEY,
+			event_type VARCHAR(64),
+			time_stamp TIMESTAMP,
+			key TEXT UNIQUE,
+			value TEXT
+		);`
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		_, err := l.db.ExecContext(ctx, query)
+		if err != nil {
+			return fmt.Errorf("failed to create strings table %w", err)
+		}
+	}
+	{
+		query := `CREATE TABLE IF NOT EXISTS maps_table (
+			id INTEGER PRIMARY KEY,
+			event_type VARCHAR(64),
+			time_stamp TIMESTAMP,
+			hash_key TEXT UNIQUE,
+			key TEXT UNIQUE,
+			value TEXT
+		);`
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		_, err := l.db.ExecContext(ctx, query)
+		if err != nil {
+			return fmt.Errorf("failed to create maps table %w", err)
+		}
+	}
+
+	return nil
 }
 
 func (l *PostgresTransactionLogger) writeTransaction(evenType EventType, storageType StorageType, key string, values ...interface{}) {
