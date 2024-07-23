652302c63a4610cdd7d3bdad4631ed9a033c4aa3
ayevtushenko <alexey.yevtushenko@ubisoft.com>
2024-07-23 20:00:21 +0200

    made the int transactions test more generic

--- pkg/kvs/transactions_test.go
+++ pkg/kvs/transactions_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -15,6 +16,23 @@ import (
 
 const hostPort = 6060
 const psqlPassword = "12345"
+
+func writeTxnFromEvent(txnLogger *PostgresTxnLogger, event *Event) {
+	txnLogger.WriteTransaction(event.txnType, event.storageType, event.key, event.value)
+}
+
+// This verficiation mechanism doesn't take into a consideration the timestamp
+// If exactly the same event was inserted later, the code will break.
+func hasEvent(eventList []Event, src *Event) bool {
+	for _, event := range eventList {
+		if event.key == src.key && event.txnType == src.txnType && event.storageType == src.storageType {
+			if reflect.ValueOf(event.value).Equal(reflect.ValueOf(src.value)) {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func TestMain(m *testing.M) {
 	var tearDown bool
@@ -52,11 +70,17 @@ func TestIntTransaction(t *testing.T) {
 		txnLogger.WaitForPendingTransactions()
 	}()
 
-	const eventKey = "number"
-	const eventValue int32 = 1777998
+	i32Put := Event{storageType: storageInt, txnType: txnPut, key: "_i32_key", value: int32(1777998)}
+	i32Get := Event{storageType: storageInt, txnType: txnGet, key: "_i32_key"}
+	i32Del := Event{storageType: storageInt, txnType: txnDelete, key: "_i32_key"}
 
-	txnLogger.WriteTransaction(txnPut, storageInt, "number", eventValue)
-	txnLogger.WriteTransaction(txnGet, storageInt, "number", nil)
+	writeTxnFromEvent(txnLogger, &i32Put)
+	writeTxnFromEvent(txnLogger, &i32Get)
+	writeTxnFromEvent(txnLogger, &i32Del)
+
+	// f32PutEvent := Event{txnType: txnPut, key: "_f32_key", value: 3.14}
+	// f32GetEvent := Event{txnType: txnGet, key: "_f32_key"}
+	// f32DelEvent := Event{txnType: txnDelete, key: "_f32_key"}
 
 	// Make sure that events we properly inserted into the database.
 	// This is an emulation of a real-world scenario, where the data is already in a database
@@ -79,11 +103,15 @@ func TestIntTransaction(t *testing.T) {
 			}
 		}
 	}
-	assert.Equal(t, 2, len(eventList))
-	assert.Equal(t, eventKey, eventList[0].key)
-	assert.Equal(t, txnPut, eventList[0].txnType)
-	assert.Equal(t, eventValue, eventList[0].value.(int32))
 
-	assert.Equal(t, txnGet, eventList[1].txnType)
-	assert.Equal(t, eventKey, eventList[1].key)
+	assert.Equal(t, 3, len(eventList))
+	assert.True(t, hasEvent(eventList, &i32Put))
+	assert.True(t, hasEvent(eventList, &i32Get))
+	assert.True(t, hasEvent(eventList, &i32Del))
+}
+
+// Decided to separate testing map transactions from other types
+// since they are the most complex.
+func TestMapTransactions(t *testing.T) {
+
 }
