fb40891de7b7eb37bf6b4034077897bbf1ff1c7c
ayevtushenko <alexey.yevtushenko@ubisoft.com>
2024-08-04 10:59:26 +0200

    started sketching out transaction service in main

--- services/txn/main.go
+++ services/txn/main.go
@@ -1,5 +1,38 @@
 package txn
 
-func main() {
+import (
+	"flag"
 
+	"github.com/isnastish/kvs/proto/api"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// ///////////////////////////////////api type aliases/////////////////////////////////////
+type APIWriteTransactionStream api.TransactionService_WriteTransactionsServer
+type APIReadTransactionStream api.TransactionService_ReadTransactionsServer
+type APIProcessErrorStream api.TransactionService_ProcessErrorsServer
+
+type TransactionService struct {
+	api.UnimplementedTransactionServiceServer
+}
+
+func (s *TransactionService) ReadTransactions(_ *emptypb.Empty, stream APIReadTransactionStream) error {
+	return nil
+}
+
+func (s *TransactionService) WriteTransactions(stream APIWriteTransactionStream) error {
+	return nil
+}
+
+func (s *TransactionService) ProcessErrors(_ *emptypb.Empty, stream APIProcessErrorStream) error {
+	return nil
+}
+
+func main() {
+	/////////////////////////////////////Transaction service port/////////////////////////////////////
+	txnPort := flag.Uint("port", 5051, "Transaction service listening port")
+	flag.Parse()
+
+	_ = txnPort
 }
