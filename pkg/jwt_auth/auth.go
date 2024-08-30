e4e255099cfb1911e72db41dd38b1a69b34c7687
isnastish <paukden12@gmail.com>
2024-08-30 19:49:54 +0200

    added jwt-auth package

---
+++ pkg/jwt_auth/auth.go
@@ -0,0 +1,5 @@
+package jwtauth
+
+// TODO: Try out ED25519 instead of RSA for generating private/public key pairs.
+// The generated keys are usually smaller and more secure.
+// openssl genpkey -algorithm ED25519 ...
--- services/kvs/main.go
+++ services/kvs/main.go
@@ -25,10 +25,6 @@ import (
 	_ "github.com/google/uuid"
 )
 
-// TODO: Try out ED25519 instead of RSA for generating private/public key pairs.
-// The generated keys are usually smaller and more secure.
-// openssl genpkey -algorithm ED25519 ...
-
 type JWTClaims struct {
 	// We use username and password, but it could be anything,
 	// for example a project name, namespace etc.
