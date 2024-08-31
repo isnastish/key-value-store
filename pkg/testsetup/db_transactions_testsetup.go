6d1f9db235b1df3bccc53fa6882d4c33e9c43747
isnastish <paukden12@gmail.com>
2024-08-31 16:57:24 +0200

    added logs

--- pkg/testsetup/db_transactions_testsetup.go
+++ pkg/testsetup/db_transactions_testsetup.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 )
 
-var postgresDockerImage = "postgres:16.3"
-
-func StartPostgresContainer(hostPort int, password string) (bool, error) {
+func StartPostgresContainer(port int) (bool, error) {
 	expectedOutput := "PostgreSQL init process complete; ready for start up"
 	return startDockerContainer(
 		expectedOutput,
-		"docker", "run", "--rm", "--name", "postgres-emulator", "-p", fmt.Sprintf("127.0.0.1:%d:5432", hostPort),
-		"-e", fmt.Sprintf("POSTGRES_PASSWORD=%s", password), postgresDockerImage,
+		"docker", "run", "--rm", "--name", "postgres-emulator", "-p", fmt.Sprintf("127.0.0.1:%d:5432", port),
+		"-e", "POSTGRES_PASSWORD=saml", "postgres:16.3",
 	)
 }
 
