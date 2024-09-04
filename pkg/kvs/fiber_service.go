e3825c29c61b3d2efdda9db7af3b6232c3efe585
isnastish <paukden12@gmail.com>
2024-09-04 13:28:38 +0200

    using fiber package

--- pkg/kvs/fiber_service.go
+++ pkg/kvs/fiber_service.go
@@ -1,7 +1,14 @@
 // Implementation of the key-value storage service using fiber.
 package kvs
 
-import "github.com/isnastish/kvs/pkg/apitypes"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+
+	"github.com/isnastish/kvs/pkg/apitypes"
+	info "github.com/isnastish/kvs/pkg/serviceinfo"
+)
 
 type FiberService struct {
 	storage map[apitypes.TransactionStorageType]Storage
@@ -18,4 +25,21 @@ func NewFiberService() *FiberService {
 	service.storage[apitypes.StorageMap] = newMapStorage()
 
 	return service
+}
+
+func (s *FiberService) Server() error {
+	app := fiber.New(fiber.Config{
+		ServerHeader: "fiber",
+	})
+
+	// testing GET method.
+	app.Get(fmt.Sprintf("/%s/%s/uint/put/{key:[0-9A-Za-z_]+}", info.ServiceName(), info.ServiceVersion()), func(ctx fiber.Ctx) error {
+		return ctx.SendString("hello world from fiber")
+	})
+
+	if err := app.Server().ListenAndServe("0.0.0.0:4000"); err != nil {
+		return fmt.Errorf("fiber: failed to listen on port 0.0.0.0:4000")
+	}
+
+	return nil
 }
